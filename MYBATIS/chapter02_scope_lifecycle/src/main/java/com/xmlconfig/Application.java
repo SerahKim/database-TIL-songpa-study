c59e2550fe23241e4b731097c9a82fe50ccbf9c5
sieun <sieunuies@gmail.com>
2025-05-23 14:52:51 +0900

    [250523] TIL : mybatis-scop_lifecycle

---
+++ MYBATIS/chapter02_scope_lifecycle/src/main/java/com/xmlconfig/Application.java
@@ -0,0 +1,13 @@
+package com.xmlconfig;
+
+import static com.xmlconfig.Template.getSqlSession;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+    }
+}
